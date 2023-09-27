faca2116e929deae473243aa43b3704bfaf2204e
Zipper228 <109089687+Zipper228@users.noreply.github.com>
2023-09-28 01:30:26 +0300

    Update db.js

--- dist/db.js
+++ dist/db.js
@@ -18,7 +18,7 @@ export async function createTableUsers() {
     try {
         await pool.query("CREATE TABLE IF NOT EXISTS " +
             process.env.MYSQL_TABLE_NAME_USERS +
-            " ( teleg_id INTEGER PRIMARY KEY NOT NULL, address VARCHAR(35) NOT NULL, share FLOAT NOT NULL)");
+            " (address VARCHAR(50) NOT NULL, teleg_id INT NOT NULL, username VARCHAR(40), share FLOAT NOT NULL, balance FLOAT NOT NULL, PRIMARY KEY (teleg_id))");
     }
     catch (error) {
         console.log("ERROR IN CREATETABLEUSERS " + error);
@@ -36,12 +36,33 @@ export async function createTableLogs() {
         console.log("ERROR IN CREATETABLELOGS " + error);
     }
 }
+export async function createTableTrans() {
+    try {
+        await pool.query("CREATE TABLE IF NOT EXISTS " +
+            process.env.MYSQL_TABLE_NAME_TRANS +
+            " (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, hash VARCHAR(70) NOT NULL)");
+    }
+    catch (error) {
+        console.log("ERROR IN CREATETABLETRANS " + error);
+    }
+}
 export async function getUser(id) {
     try {
         const [result] = await pool.query("SELECT * FROM " +
             process.env.MYSQL_TABLE_NAME_USERS +
             " WHERE teleg_id = ?", [id]);
-        return JSON.parse(JSON.stringify(result));
+        return JSON.parse(JSON.stringify(result))[0];
+    }
+    catch (error) {
+        console.log("ERROR IN GETuser " + error);
+    }
+}
+export async function getUserByName(username) {
+    try {
+        const [result] = await pool.query("SELECT teleg_id FROM " +
+            process.env.MYSQL_TABLE_NAME_USERS +
+            " WHERE username = ?", [username]);
+        return JSON.parse(JSON.stringify(result))[0];
     }
     catch (error) {
         console.log("ERROR IN GETuser " + error);
@@ -58,11 +79,36 @@ export async function getLogs(user_id) {
         console.log("ERROR IN GETLOGS " + error);
     }
 }
-export async function createLog(user_id, datetime, value) {
+export async function getTrans(hash) {
+    try {
+        const result = await pool.query("SELECT * FROM " + process.env.MYSQL_TABLE_NAME_TRANS + " WHERE hash = ?", [hash]);
+        let resultOne = JSON.parse(JSON.stringify(result));
+        return resultOne[0];
+    }
+    catch (error) {
+        console.log("ERROR IN GETTRANS " + error);
+    }
+}
+export async function usableBalance(balance) {
+    try {
+        var [balances] = await pool.query("SELECT balance FROM " + process.env.MYSQL_TABLE_NAME_USERS);
+        var result = JSON.parse(JSON.stringify(balances));
+        for (let i = 0; i < result.length; i++) {
+            balance -= result[i].balance;
+            console.log(result[i].balance);
+        }
+        return balance;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+export async function createLog(user_id, value) {
+    const dateTime = datetime();
     try {
         const result = await pool.query("INSERT INTO " +
             process.env.MYSQL_TABLE_NAME_LOGS +
-            "(user_id, datetime, value) VALUES (?, ?, ?)", [user_id, datetime, value]);
+            "(user_id, datetime, value) VALUES (?, ?, ?)", [user_id, dateTime, value]);
         var [bydate] = await pool.query("SELECT id FROM " +
             process.env.MYSQL_TABLE_NAME_LOGS +
             " WHERE user_id = ? ORDER BY id DESC LIMIT 10 OFFSET " +
@@ -85,11 +131,48 @@ export async function createLog(user_id, datetime, value) {
         console.log("ERROR IN CREATELOG " + error);
     }
 }
-export async function createUser(teleg_id, address, share) {
+export async function createTrans(hash) {
     try {
+        const result = await pool.query("INSERT INTO " + process.env.MYSQL_TABLE_NAME_TRANS + "(hash) VALUES (?)", [hash]);
+        var [bydate] = await pool.query("SELECT id FROM " +
+            process.env.MYSQL_TABLE_NAME_TRANS +
+            " WHERE hash = ? ORDER BY id DESC LIMIT 10 OFFSET " +
+            process.env.MYSQL_TRANS_SAVE_NUM, [hash]);
+        const bydateRes = JSON.parse(JSON.stringify(bydate));
+        if (bydateRes.length === 0) {
+            return [JSON.parse(JSON.stringify(result)), bydateRes];
+        }
+        var stringRequest = "DELETE FROM " + process.env.MYSQL_TABLE_NAME_TRANS + " WHERE";
+        for (let i = 0; i < bydateRes.length; i++) {
+            if (i !== 0) {
+                stringRequest += " OR";
+            }
+            stringRequest += " id = " + String(bydateRes[i].id);
+        }
+        await pool.query(stringRequest);
+        return JSON.parse(JSON.stringify(result));
+    }
+    catch (error) {
+        console.log("ERROR IN CREATETRANS " + error);
+    }
+}
+export async function deleteUser(teleg_id) {
+    try {
+        await pool.query("DELETE FROM " + process.env.MYSQL_TABLE_NAME_LOGS + " WHERE user_id = ?", [teleg_id]);
+        await pool.query("DELETE FROM " +
+            process.env.MYSQL_TABLE_NAME_USERS +
+            " WHERE teleg_id = ?", [teleg_id]);
+    }
+    catch (error) {
+        console.log("ERROR IN DELETEUSER " + error);
+    }
+}
+export async function createUser(teleg_id, address, username, share) {
+    try {
+        const shareUpdated = share || 0.15;
         const result = await pool.query("INSERT INTO " +
             process.env.MYSQL_TABLE_NAME_USERS +
-            "(teleg_id, address, share) VALUES (?, ?, ?)", [teleg_id, address, share]);
+            " (teleg_id, address, username, share, balance) VALUES (?, ?, ?, ?, ?)", [teleg_id, address, username, shareUpdated, 0]);
         return JSON.parse(JSON.stringify(result));
     }
     catch (error) {
@@ -97,27 +180,56 @@ export async function createUser(teleg_id, address, share) {
     }
 }
 export async function updateUsers(teleg_id, toUpdate, newValue) {
+    console.log(newValue);
+    console.log("UPDATE " +
+        process.env.MYSQL_TABLE_NAME_USERS +
+        " SET " +
+        toUpdate +
+        " = " +
+        newValue +
+        " WHERE teleg_id = " +
+        teleg_id);
     try {
         await pool.query("UPDATE " +
             process.env.MYSQL_TABLE_NAME_USERS +
             " SET " +
             toUpdate +
-            " = ? WHERE id = ?", [newValue, teleg_id]);
+            " = " +
+            newValue +
+            " WHERE teleg_id = " +
+            teleg_id);
     }
     catch (error) {
         console.log("ERROR IN UPDATEUSERS " + error);
     }
 }
-createTableUsers();
-createTableLogs();
-//await createUser(123, "hhh", 0.15);
-for (let i = 0; i < 3; i++) {
-    var time = datetime();
-    await createLog(123, time, i + 0.15);
-    console.log(await getLogs(123));
+export async function updateBalance(teleg_id, value, increase) {
+    const user = await getUser(teleg_id);
+    var balance = user.balance;
+    console.log(balance);
+    if (increase) {
+        balance += value;
+    }
+    else {
+        balance -= value;
+    }
+    await updateUsers(teleg_id, "balance", balance);
 }
-for (let i = 0; i < 3; i++) {
-    var time = datetime();
-    await createLog(123, time, i + 0.15);
-    console.log(await getLogs(123));
+export async function dropTable(table) {
+    console.log("DROP TABLE " + table);
+    await pool.query("DROP TABLE " + table);
 }
+export async function checkUser(teleg_id) {
+    const user = await getUser(teleg_id);
+    if (user !== undefined) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+//await dropTable(process.env.MYSQL_TABLE_NAME_LOGS || "");
+//await dropTable(process.env.MYSQL_TABLE_NAME_TRANS || "");
+//await createTableTrans();
+//deleteUser(875460557);
+//console.log(await usableBalance(100));
